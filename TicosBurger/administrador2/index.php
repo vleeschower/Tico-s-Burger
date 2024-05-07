be97feb177642eaaffc786525590b8e6ee55691e
vleeschower <144490940+vleeschower@users.noreply.github.com>
2024-05-06 19:35:27 -0600

    Quite paginas de error

--- TicosBurger/administrador2/index.php
+++ TicosBurger/administrador2/index.php
@@ -42,12 +42,12 @@ $nombre=$_SESSION['Nombre'];
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <div class="sb-sidenav-menu-heading">Administrar</div>
                             <!-- Cambiar link-->
                             <a class="nav-link" href="index.html">
                                 <!-- icono de Dashboard-->
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                                Administradores
                             </a>
                             <div class="sb-sidenav-menu-heading">Interface</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
